cd36d26144363e5f7c640bb14915d98cd8f61bd3
Jon Eubank <joneubank@gmail.com>
2018-03-02 11:35:38 -0500

    Cavatica Proxy module template, no functionality

---
+++ cavatica-proxy/src/main/java/io/kidsfirst/keys/cavatica/proxy/CavaticaProxy.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 Ontario Institute for Cancer Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.kidsfirst.keys.cavatica.proxy;
+
+import io.kidsfirst.keys.core.LambdaRequestHandler;
+import io.kidsfirst.keys.core.dao.SecretDao;
+import io.kidsfirst.keys.core.model.Secret;
+import io.kidsfirst.keys.core.utils.KMSUtils;
+import org.json.simple.JSONObject;
+
+import java.util.List;
+
+public class CavaticaProxy extends LambdaRequestHandler {
+
+
+  @Override
+  public String processEvent(JSONObject event, String userId) throws IllegalArgumentException {
+
+    return event.toJSONString();
+    
+  }
+
+}
