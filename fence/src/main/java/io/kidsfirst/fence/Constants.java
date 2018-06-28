d7cdad3f76097b75fb6272046d477200f8967773
Grant Guo <gguo@olm-gguo.oicr.on.ca>
2018-06-28 16:57:38 -0400

    cleaning unused stuff

--- fence/src/main/java/io/kidsfirst/fence/Constants.java
+++ fence/src/main/java/io/kidsfirst/fence/Constants.java
@@ -3,8 +3,6 @@ package io.kidsfirst.fence;
 public interface Constants {
     String ENV_CLIENT_INFO_TABLE_NAME = "KF_LAMBDA_AUTH_CLIENT_TABLE_NAME";
     String ENV_FENCE_TOKEN_ENDPOINT = "KF_LAMBDA_FENCE_TOKEN_ENDPOINT";
-    String ENV_FENCE_PUBLIC_KEY = "KF_LAMBDA_FENCE_PUBLIC_KEY";
-    String ENV_EGO_PUBLIC_KEY = "KF_LAMBDA_EGO_PUBLIC_KEY";
     String ENV_TOKEN_TABLE_NAME = "KF_LAMBDA_TOKEN_TABLE_NAME";
     String DEFAULT_CLIENT_INFO_TABLE_NAME = "kf_auth_client";
     String DEFAULT_TOKEN_TABLE_NAME = "kf_token";
@@ -18,7 +16,5 @@ public interface Constants {
     String FIELD_NAME_OF_USER_ID_IN_FENCE = "userid_in_fence";
     String FIELD_NAME_OF_ACCESS_TOKEN = "access_token";
     String FIELD_NAME_OF_REFRESH_TOKEN = "refresh_token";
-    String FIELD_NAME_OF_ID_TOKEN = "id_token";
-    String FIELD_NAME_OF_EGO_JWT = "EGO_JWT";
     
 }
