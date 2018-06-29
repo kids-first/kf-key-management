e09902fe66c85b76d2ad496a7a62752a717a6d87
Grant Guo <gguo@olm-gguo.oicr.on.ca>
2018-06-29 13:14:35 -0400

    use LambdaResponse to wrap data in RequestAuthorizationClient

--- fence/src/main/java/io/kidsfirst/fence/RequestAuthorizationClient.java
+++ fence/src/main/java/io/kidsfirst/fence/RequestAuthorizationClient.java
@@ -2,23 +2,37 @@ package io.kidsfirst.fence;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import io.kidsfirst.keys.core.model.LambdaResponse;
+import org.json.simple.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class RequestAuthorizationClient implements RequestStreamHandler {
 
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-        outputStream.write(
-                String.format(
-                        "{\"client_id\": \"%s\",\"redirect_uri\": \"%s\",\"scope\": \"%s\"}",
-                        Utils.getAuthClient().getClientId(),
-                        Utils.getAuthClient().getRedirectUri(),
-                        Utils.getAuthClient().getScope()
-                ).getBytes()
-        );
-        outputStream.flush();
-        outputStream.close();
+
+        LambdaResponse resp = new LambdaResponse();
+        resp.setHeaders(new HashMap<>());
+        resp.setBase64Encoded(false);
+        resp.setBody(String.format(
+                "{\"client_id\": \"%s\",\"redirect_uri\": \"%s\",\"scope\": \"%s\"}",
+                Utils.getAuthClient().getClientId(),
+                Utils.getAuthClient().getRedirectUri(),
+                Utils.getAuthClient().getScope()
+        ));
+        resp.setStatusCode(HttpURLConnection.HTTP_OK);
+
+        JSONObject responseJson = resp.toJson();
+
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+        writer.write(responseJson.toJSONString());
+        writer.close();
     }
 }
