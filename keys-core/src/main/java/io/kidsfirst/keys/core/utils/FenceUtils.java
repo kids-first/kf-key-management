8d7d0179c84237735b274351a4b649ab4dade33b
Jon Eubank <joneubank@gmail.com>
2019-01-21 15:11:53 -0500

    Add FenceUtils with Providers enum

---
+++ keys-core/src/main/java/io/kidsfirst/keys/core/utils/FenceUtils.java
@@ -0,0 +1,47 @@
+package io.kidsfirst.keys.core.utils;
+
+import io.kidsfirst.keys.core.dao.SecretDao;
+import io.kidsfirst.keys.core.model.Secret;
+
+import java.util.List;
+
+public class FenceUtils {
+
+  public enum Provider {
+    DCF,
+    GEN3;
+
+    public String accessTokenKey() {
+      return String.format("fence_%s_access",this.name()).toLowerCase();
+    }
+
+    public String refreshTokenKey() {
+      return String.format("fence_%s_refresh",this.name()).toLowerCase();
+    }
+  }
+
+  public static Provider getProvider(final String key) throws IllegalArgumentException {
+    return Provider.valueOf(key.toUpperCase());
+
+  }
+
+  public static String getAccessToken(final Provider fence, final String userId) {
+    List<Secret> secrets = SecretDao.getSecret(fence.accessTokenKey(), userId);
+    if (!secrets.isEmpty()) {
+      return secrets.get(0).getSecret();
+
+    }
+
+    return "";
+  }
+
+  public static String getRefreshToken(final Provider fence, final String userId) {
+    List<Secret> secrets = SecretDao.getSecret(fence.refreshTokenKey(), userId);
+    if (!secrets.isEmpty()) {
+      return secrets.get(0).getSecret();
+
+    }
+
+    return "";
+  }
+}
