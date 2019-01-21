0e65acc3196998981b86ba01bdd490e1236b2009
Jon Eubank <joneubank@gmail.com>
2019-01-21 16:57:19 -0500

    Refactor Request Auth Client with Request/Response objects

--- fence/src/main/java/io/kidsfirst/fence/RequestAuthorizationClient.java
+++ fence/src/main/java/io/kidsfirst/fence/RequestAuthorizationClient.java
@@ -1,41 +1,33 @@
 package io.kidsfirst.fence;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import io.kidsfirst.keys.core.LambdaRequestHandler;
+import io.kidsfirst.keys.core.model.LambdaRequest;
 import io.kidsfirst.keys.core.model.LambdaResponse;
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 
-public class RequestAuthorizationClient implements RequestStreamHandler {
+public class RequestAuthorizationClient extends LambdaRequestHandler {
 
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+  public LambdaResponse processEvent(final LambdaRequest request) throws IllegalAccessException, IllegalArgumentException, IOException {
 
-        LambdaResponse resp = new LambdaResponse();
-        HashMap<String, String> headers = new HashMap<>();
-        headers.put("Access-Control-Allow-Origin", "*");
-        resp.setHeaders(headers);
-        resp.setBase64Encoded(false);
-        resp.setBody(String.format(
-                "{\"client_id\": \"%s\",\"redirect_uri\": \"%s\",\"scope\": \"%s\"}",
-                Utils.getAuthClient().getClientId(),
-                Utils.getAuthClient().getRedirectUri(),
-                Utils.getAuthClient().getScope()
-        ));
-        resp.setStatusCode(HttpURLConnection.HTTP_OK);
+    //No UserID check - no auth required
 
-        JSONObject responseJson = resp.toJson();
+    LambdaResponse resp = new LambdaResponse();
+    resp.addDefaultHeaders();
 
-        OutputStreamWriter writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
-        writer.write(responseJson.toJSONString());
-        writer.close();
-    }
+    JSONObject body = new JSONObject();
+
+      body.put("client_id", Utils.getAuthClient().getClientId());
+      body.put("redirect_uri", Utils.getAuthClient().getRedirectUri());
+      body.put("scope", Utils.getAuthClient().getScope());
+
+      resp.setBody(body.toJSONString());
+
+    resp.setStatusCode(HttpURLConnection.HTTP_OK);
+
+    return resp;
+  }
 }
