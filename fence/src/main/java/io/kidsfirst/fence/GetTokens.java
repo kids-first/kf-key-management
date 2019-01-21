91b2abaf2c81fc596543baef1fd49768fa96a4f2
Jon Eubank <joneubank@gmail.com>
2019-01-21 15:12:43 -0500

    Convert fence-get to Request/Response Objects and add fence provider param

--- fence/src/main/java/io/kidsfirst/fence/GetTokens.java
+++ fence/src/main/java/io/kidsfirst/fence/GetTokens.java
@@ -1,16 +1,40 @@
 package io.kidsfirst.fence;
 
 import io.kidsfirst.keys.core.LambdaRequestHandler;
+import io.kidsfirst.keys.core.model.LambdaRequest;
+import io.kidsfirst.keys.core.model.LambdaResponse;
+import io.kidsfirst.keys.core.utils.FenceUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
+
+import java.net.HttpURLConnection;
+import java.util.HashMap;
+
 import static io.kidsfirst.fence.Utils.*;
 
 public class GetTokens extends LambdaRequestHandler {
-    @Override
-    public String processEvent(JSONObject event, String userId) throws IllegalArgumentException, ParseException {
 
-        KfTokens tokens = retrieveTokens(userId);
+  @Override
+  public LambdaResponse processEvent(final LambdaRequest request) throws IllegalAccessException, IllegalArgumentException {
 
-        return String.format("{\"access_token\":\"%s\", \"refresh_token\":\"%s\"}", tokens.getAccess_token(), tokens.getRefresh_token());
-    }
+    String userId = request.getUserId();
+
+    LambdaResponse resp = new LambdaResponse();
+    resp.addDefaultHeaders();
+
+    String fenceKey = request.getQueryStringValue("fence");
+    FenceUtils.Provider fence = FenceUtils.getProvider(fenceKey);
+
+    String accessToken = FenceUtils.getAccessToken(fence, userId);
+    String refreshToken = FenceUtils.getRefreshToken(fence, userId);
+
+    JSONObject body = new JSONObject();
+    body.put("access_token", accessToken);
+    body.put("refresh_token", refreshToken);
+
+    resp.setBody(body.toJSONString());
+    resp.setStatusCode(HttpURLConnection.HTTP_OK);
+    return resp;
+
+  }
 }
