35fd96c315c52f9074369d83ec041cf759e73b14
Jon Eubank <joneubank@gmail.com>
2019-02-04 20:22:53 -0500

    Use lombok val and clean service code

--- fence/src/main/java/io/kidsfirst/fence/GetTokens.java
+++ fence/src/main/java/io/kidsfirst/fence/GetTokens.java
@@ -4,34 +4,30 @@ import io.kidsfirst.keys.core.LambdaRequestHandler;
 import io.kidsfirst.keys.core.model.LambdaRequest;
 import io.kidsfirst.keys.core.model.LambdaResponse;
 import io.kidsfirst.keys.core.utils.FenceUtils;
+import lombok.val;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.ParseException;
 
 import java.net.HttpURLConnection;
-import java.util.HashMap;
-
-import static io.kidsfirst.fence.Utils.*;
 
 public class GetTokens extends LambdaRequestHandler {
 
   @Override
   public LambdaResponse processEvent(final LambdaRequest request) throws IllegalAccessException, IllegalArgumentException {
 
-    String userId = request.getUserId();
+    val userId = request.getUserId();
 
-    LambdaResponse resp = new LambdaResponse();
+    val fenceKey = request.getQueryStringValue("fence");
+    val fence = FenceUtils.getProvider(fenceKey);
+
+    val accessToken = FenceUtils.fetchAccessToken(fence, userId);
+    val refreshToken = FenceUtils.fetchRefreshToken(fence, userId);
+
+    val body = new JSONObject();
+    body.put("access_token", accessToken.orElse(""));
+    body.put("refresh_token", refreshToken.orElse(""));
+
+    val resp = new LambdaResponse();
     resp.addDefaultHeaders();
-
-    String fenceKey = request.getQueryStringValue("fence");
-    FenceUtils.Provider fence = FenceUtils.getProvider(fenceKey);
-
-    String accessToken = FenceUtils.getAccessToken(fence, userId);
-    String refreshToken = FenceUtils.getRefreshToken(fence, userId);
-
-    JSONObject body = new JSONObject();
-    body.put("access_token", accessToken);
-    body.put("refresh_token", refreshToken);
-
     resp.setBody(body.toJSONString());
     resp.setStatusCode(HttpURLConnection.HTTP_OK);
     return resp;
