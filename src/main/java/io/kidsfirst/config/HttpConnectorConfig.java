99fe73a987493244fd2357cdd308373719bc9ad6
jecos <jcostanza@ferlab.bio>
2021-02-23 13:26:03 -0500

    Disable ssl

--- src/main/java/io/kidsfirst/config/HttpConnectorConfig.java
+++ src/main/java/io/kidsfirst/config/HttpConnectorConfig.java
@@ -11,7 +11,7 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class HttpConnectorConfig {
     @Value("${server.port}")
     private int httpsPort;
