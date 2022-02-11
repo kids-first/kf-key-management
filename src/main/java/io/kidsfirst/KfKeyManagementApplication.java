db4e55ef52113f3208f5e29891589e41e7055a82
jecos <jcostanza@ferlab.bio>
2022-02-11 11:31:28 -0500

    fix : Enable zuul proxy on application

--- src/main/java/io/kidsfirst/KfKeyManagementApplication.java
+++ src/main/java/io/kidsfirst/KfKeyManagementApplication.java
@@ -3,7 +3,9 @@ package io.kidsfirst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class KfKeyManagementApplication {
 
