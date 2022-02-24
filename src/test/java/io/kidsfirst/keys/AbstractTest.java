cea5e031a8baaa9de5a8a89615f7c0e459562061
jecos <jcostanza@ferlab.bio>
2022-02-24 08:51:58 -0500

    refactor: split test into many files

--- src/test/java/io/kidsfirst/keys/AbstractTest.java
+++ src/test/java/io/kidsfirst/keys/AbstractTest.java
@@ -1,65 +1,89 @@
 package io.kidsfirst.keys;
 
-import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
+import io.kidsfirst.core.model.Secret;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lombok.val;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.extension.RegisterExtension;
+import org.keycloak.admin.client.CreatedResponseUtil;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClientBuilder;
 
+import java.net.URI;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.function.Consumer;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ActiveProfiles("dev")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
 @AutoConfigureWebTestClient(timeout = "5000")
-public abstract class AbstractTest{
+public abstract class AbstractTest {
 
-    private static final int DYNAMODB_PORT = 8000;
+    public static final String CLIENT_SECRET = "secret";
+    public static final String REALM_NAME = "master";
+    protected static final int DYNAMODB_PORT = 8000;
+    protected final static String KF_CLIENT_ID = "kf";
+    protected static DynamoDbEnhancedAsyncClient dynamoClient;
+    protected static DynamoDbAsyncTable<Secret> secretTable;
+    public static GenericContainer<?> dynamodb = new GenericContainer<>("amazon/dynamodb-local:latest")
+            .withExposedPorts(DYNAMODB_PORT);
+
+    public static KeycloakContainer keycloak = new KeycloakContainer()
+            .withAdminUsername("admin")
+            .withAdminPassword("admin");
+
+    static {
+        keycloak.start();
+        dynamodb.start();
+        init();
+    }
+
+    static void init() {
+        DynamoDbAsyncClientBuilder builder = DynamoDbAsyncClient.builder();
+        builder.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test"))).region(Region.US_EAST_1)
+                .endpointOverride(URI.create("http://localhost:" + dynamodb.getFirstMappedPort()));
+        dynamoClient = DynamoDbEnhancedAsyncClient.builder().dynamoDbClient(builder.build())
+                .build();
+
+        try {
+            secretTable = dynamoClient.table("kf-key-management-secret", TableSchema.fromBean(Secret.class));
+            secretTable.createTable().get();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        createKeycloakClient(KF_CLIENT_ID);
+
+    }
 
     @Autowired
     protected Environment env;
 
     @Autowired
     protected WebTestClient webClient;
-
-
-    @RegisterExtension
-    static WireMockExtension cavaticaWM = WireMockExtension.newInstance()
-            .options(wireMockConfig().dynamicPort())
-            .build();
-
-    @RegisterExtension
-    static WireMockExtension gen3VM = WireMockExtension.newInstance()
-            .options(wireMockConfig().dynamicPort())
-            .build();
-
-    protected Consumer<String> matchValues(String... others) {
-        return s -> MatcherAssert.assertThat(Arrays.asList(s.split(",")), Matchers.hasItems(others));
-    }
-
-
-    @Container
-    public static GenericContainer<?> dynamodb = new GenericContainer<>("amazon/dynamodb-local:latest")
-            .withExposedPorts(DYNAMODB_PORT);
-
-    @Container
-    public static KeycloakContainer keycloak = new KeycloakContainer()
-            .withAdminUsername("admin")
-            .withAdminPassword("admin");
 
     @DynamicPropertySource
     static void setDynamicKeycloakPort(DynamicPropertyRegistry registry) {
@@ -73,20 +97,68 @@ public abstract class AbstractTest{
                 () -> String.format("http://localhost:%d", dynamodb.getFirstMappedPort()));
     }
 
-    @DynamicPropertySource
-    static void setCavaticaUri(DynamicPropertyRegistry registry) {
-        registry.add("application.cavatica_root",
-                () -> cavaticaWM.baseUrl());
+    protected Consumer<String> matchValues(String... others) {
+        return s -> MatcherAssert.assertThat(Arrays.asList(s.split(",")), Matchers.hasItems(others));
     }
 
-    @DynamicPropertySource
-    static void setGen3Uri(DynamicPropertyRegistry registry) {
-        registry.add("application.fence.gen3.token_endpoint",
-                () -> gen3VM.baseUrl());
+    protected static UserIdAndToken createUserAndSecretAndObtainAccessToken(String service, String secret) {
+        val username = RandomStringUtils.random(10, true, false);
+        val password = RandomStringUtils.random(10, true, false);
+        val userId = createKeycloakUser(username, password, "test" + username + "@test.org", RandomStringUtils.random(10, true, false), RandomStringUtils.random(10, true, false));
+        createSecret(service, userId, secret);
+        String accessToken = obtainAccessToken(username, password);
+
+        return new UserIdAndToken(userId, accessToken);
     }
 
+    public static void createSecret(String service, String userId, String secret) {
+        dynamoClient.table("kf-key-management-secret", TableSchema.fromBean(Secret.class)).putItem(new Secret(userId, service, secret));
+    }
 
+    public static String createKeycloakUser(String username, String password, String email, String firstName, String lastName) {
+        UserRepresentation user = new UserRepresentation();
+        user.setEnabled(true);
+        user.setUsername(username);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
 
+        CredentialRepresentation passwordCred = new CredentialRepresentation();
+        passwordCred.setTemporary(false);
+        passwordCred.setType(CredentialRepresentation.PASSWORD);
+        passwordCred.setValue(password);
+
+        user.setCredentials(Collections.singletonList(passwordCred));
+        javax.ws.rs.core.Response response = keycloak.getKeycloakAdminClient().realm(REALM_NAME).users().create(user);
+        return CreatedResponseUtil.getCreatedId(response);
+    }
+
+    public static void createKeycloakClient(String clientId) {
+        ClientRepresentation client = new ClientRepresentation();
+        client.setClientId(clientId);
+        client.setEnabled(true);
+        client.setPublicClient(false);
+        client.setDirectAccessGrantsEnabled(true);
+        client.setRedirectUris(Collections.singletonList("*"));
+        client.setSecret(CLIENT_SECRET);
+        keycloak.getKeycloakAdminClient().realm(REALM_NAME).clients().create(client);
+    }
+
+    protected static String obtainAccessToken(String username, String password) {
+        String tokenUrl = keycloak.getAuthServerUrl() + "realms/master/protocol/openid-connect/token";
+        Response r = RestAssured.given()
+                .contentType("application/x-www-form-urlencoded; charset=utf-8")
+                .formParam("grant_type", "password")
+                .formParam("username", username)
+                .formParam("password", password)
+                .formParam("client_id", KF_CLIENT_ID)
+                .formParam("client_secret", CLIENT_SECRET)
+                .post(tokenUrl);
+
+        assertThat(HttpStatus.OK.value()).isEqualTo(r.statusCode());
+        return r.body().jsonPath().get("access_token");
+
+    }
 
 
 }
---
+++ src/test/java/io/kidsfirst/keys/KeyStoreTests.java
@@ -0,0 +1,109 @@
+package io.kidsfirst.keys;
+
+import com.nimbusds.jose.shaded.json.JSONObject;
+import io.kidsfirst.core.model.Secret;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Slf4j
+public class KeyStoreTests extends AbstractTest {
+
+    private final String keyStoreUri = "/key-store";
+    protected static String defaultAccessToken = "";
+    @BeforeAll
+    private static void initTest(){
+        val userAndToken = createUserAndSecretAndObtainAccessToken("cavatica", "cavatica_secret");
+        defaultAccessToken = userAndToken.getAccessToken();
+    }
+
+    @Test
+    void testKeyStorePreflight() {
+        webClient.options()
+                .uri(keyStoreUri)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectHeader()
+
+                .value("Allow", matchValues("PUT", "DELETE", "GET", "HEAD", "OPTIONS"));
+
+
+    }
+
+    @Test
+    void testKeyStoreGETContentTypeDifferentThanTextPlain() {
+        webClient.get()
+                .uri(keyStoreUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + defaultAccessToken)
+                .exchange()
+                .expectStatus().isEqualTo(406)
+                .expectHeader();
+
+    }
+
+    @Test
+    void testKeyStoreGET() {
+        val body = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(keyStoreUri)
+                        .queryParam("service", "cavatica")
+                        .build())
+                .accept(MediaType.TEXT_PLAIN)
+                .header("Authorization", "Bearer " + defaultAccessToken)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().returnResult().getResponseBody();
+        assert body != null;
+        assertThat(new String(body)).isEqualTo("cavatica_secret");
+    }
+
+    @Test
+    void testKeyStoreDELETE() throws ExecutionException, InterruptedException {
+        val userIdAndToken = createUserAndSecretAndObtainAccessToken("cavatica", "my_secret");
+        JSONObject body = new JSONObject();
+        body.put("service", "cavatica");
+        webClient
+                .method(HttpMethod.DELETE)
+                .uri(keyStoreUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + userIdAndToken.getAccessToken())
+                .bodyValue(body.toJSONString())
+                .exchange()
+                .expectStatus().isOk();
+        val secret = secretTable.getItem(new Secret(userIdAndToken.getUserId(), "cavatica", null)).get();
+        assertThat(secret).isNull();
+    }
+
+    @Test
+    void testKeyStorePUT() throws Exception {
+        String my_secret = "my_secret";
+        JSONObject body = new JSONObject();
+        body.put("service", "cavatica");
+        body.put("secret", my_secret);
+        val userIdAndToken = createUserAndSecretAndObtainAccessToken("cavatica", my_secret);
+        webClient
+                .put()
+                .uri(keyStoreUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + userIdAndToken.getAccessToken())
+                .bodyValue(body.toJSONString())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody();
+
+        val secret = secretTable.getItem(new Secret(userIdAndToken.getUserId(), "cavatica", null)).get();
+        assertThat(secret).isNotNull();
+        assertThat(secret.getSecret()).isEqualTo("encrypted_" + my_secret);
+    }
+
+}
